8ccbbdcd7ba30b6fa187b6eb457cd475f4645fed
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-09-27 15:04:19 +0000

    add more code to js script

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-$(document).ready(function() {
- 
+$(document).ready(function () {
 
- 	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
+
+	$("#stream1_btn").on(function () {
+		$(".stream1").fadeToggle(1000, 0.5);
 	});
-	$("#stream2_btn").on("click", function() {
+	$("#stream2_btn").on("click", function () {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream2").addClass('highlight_stream');
+		$(".stream2").addClass('highlight_stream');
 	});
-	$("#stream3_btn").on("click", function() {
+	$("#stream3_btn").on("click", function () {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream3").addClass('highlight_stream');
+		$(".stream3").addClass('highlight_stream');
 	});
 
 
-}); 
+});
